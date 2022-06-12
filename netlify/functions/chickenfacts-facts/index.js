f147571622ad6d2e6d36d1ac8603bd368829d45a
Aaron Saray <me@aaronsaray.com>
2022-06-12 12:16:31 -0500

    Updated output proxy.

--- netlify/functions/chickenfacts-facts/index.js
+++ netlify/functions/chickenfacts-facts/index.js
@@ -1,6 +1,19 @@
+const fetch = require('node-fetch')
+
 exports.handler = async () => {
+    let statusCode, data;
+
+    try {
+        const response = await fetch('https://chickenfacts.io/api/v1/facts.json');
+        data = await response.json();
+        statusCode = 200;
+    } catch (err) {
+        statusCode = err.statusCode || 500;
+        data = { error: err.message };
+    }
+
     return {
-        statusCode: 200,
-        body: JSON.stringify({ last_counted: '2022-02-03' })
+        statusCode,
+        body: JSON.stringify(data)
     }
 };
