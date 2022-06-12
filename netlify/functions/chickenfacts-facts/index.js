88b58c5207cef9b7998eae42db5122c703e85875
Aaron Saray <me@aaronsaray.com>
2022-06-12 12:35:57 -0500

    Undoing this and going with v2

--- netlify/functions/chickenfacts-facts/index.js
+++ netlify/functions/chickenfacts-facts/index.js
@@ -1,19 +1,16 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch')
 
 exports.handler = async () => {
     let statusCode, data;
 
-    // try {
-    //     const response = await fetch('https://chickenfacts.io/api/v1/facts.json');
-    //     data = await response.json();
-    //     statusCode = 200;
-    // } catch (err) {
-    //     statusCode = err.statusCode || 500;
-    //     data = { error: err.message };
-    // }
-
-    statusCode = 200;
-    data = { 'i': 'am data' }
+    try {
+        const response = await fetch('https://chickenfacts.io/api/v1/facts.json');
+        data = await response.json();
+        statusCode = 200;
+    } catch (err) {
+        statusCode = err.statusCode || 500;
+        data = { error: err.message };
+    }
 
     return {
         statusCode,
