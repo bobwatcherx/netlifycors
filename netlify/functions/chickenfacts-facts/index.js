e0265b173afba32fc64bd7d7d0144b49613fe5ca
Aaron Saray <me@aaronsaray.com>
2022-06-12 12:33:01 -0500

    troubleshooting now

--- netlify/functions/chickenfacts-facts/index.js
+++ netlify/functions/chickenfacts-facts/index.js
@@ -3,14 +3,17 @@ import fetch from 'node-fetch';
 exports.handler = async () => {
     let statusCode, data;
 
-    try {
-        const response = await fetch('https://chickenfacts.io/api/v1/facts.json');
-        data = await response.json();
-        statusCode = 200;
-    } catch (err) {
-        statusCode = err.statusCode || 500;
-        data = { error: err.message };
-    }
+    // try {
+    //     const response = await fetch('https://chickenfacts.io/api/v1/facts.json');
+    //     data = await response.json();
+    //     statusCode = 200;
+    // } catch (err) {
+    //     statusCode = err.statusCode || 500;
+    //     data = { error: err.message };
+    // }
+
+    statusCode = 200;
+    data = { 'i': 'am data' }
 
     return {
         statusCode,
